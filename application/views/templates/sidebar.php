bb4066e150aa895a74bcd5e28a1dd501392b60af
achmadqusyairii <achmadqusayiri291017@gmail.com>
2022-08-14 13:22:36 +0700

    first commit

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -11,7 +11,7 @@
         </div>
         <div class="pull-left info">
           <p>Admin</p>
-          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+          <a href="##"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
       <!-- search form -->
