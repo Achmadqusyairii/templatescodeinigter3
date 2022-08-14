d67be00909565399f88ea41d03f196d1188f3b91
achmadqusyairii <achmadqusayiri291017@gmail.com>
2022-08-14 13:12:51 +0700

    penyempurnaan

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,5 +1,5 @@
 <!-- 
-  INI YANG PALING ATAS -->
+  INI YANG PALING KANAN -->
   <!-- Left side column. contains the logo and sidebar -->
   <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
@@ -10,7 +10,7 @@
           <img src="<?php echo base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Alexander Pierce</p>
+          <p>Admin</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
@@ -28,7 +28,7 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+        <li><a href=#><i class="fa fa-usd"></i> <span>Dana Masuk</span></a></li>
         
       </ul>
     </section>
